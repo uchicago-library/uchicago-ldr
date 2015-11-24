d50c891d9450a0c2c10744a4e302d4e9145bae04
Brian Balsamo <balsamo@uchicago.edu>
2015-11-24 10:27:03 -0600

    Added the filename and the user account (best guess, not "secure") to the default log output. Additionally added another sparse output, for local output where the added fields aren't required.

--- uchicagoldrLogging/formatters.py
+++ uchicagoldrLogging/formatters.py
@@ -1,3 +1,9 @@
 def default():
     from logging import Formatter
-    return Formatter("[%(levelname)s] %(asctime)s = %(message)s",datefmt="%Y-%m-%dT%H:%M:%S") 
+    from getpass import getuser
+
+    return Formatter("[%(levelname)s] [%(asctime)s] [%(filename)s] ["+getuser()+"] = %(message)s",datefmt="%Y-%m-%dT%H:%M:%S") 
+
+def sparse():
+    from logging import Formatter
+    return Formatter("[%(levelname)s] [%(asctime)s] = %(message)s",datefmt="%Y-%m-%dT%H:%M:%S") 
